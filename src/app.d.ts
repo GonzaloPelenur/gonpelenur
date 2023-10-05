8759f4c72b031264be2930f8730e81c36ba9fddc
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-05 18:22:34 -0400

    fixed adapter errors

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,18 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface Error {}
+		interface Locals {}
+		interface PageData {}
+		interface Platform {
+		       env: {
+		           COUNTER: DurableObjectNamespace;
+			       };
+			       context: {
+			           waitUntil(promise: Promise<any>): void;
+			       };
+			       caches: CacheStorage & { default: Cache }
+			   }
 	}
 }
 
