32f413b695976717113487395a417ae1594ce5e9
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-05 20:39:39 -0400

    sunacchi

--- src/lib/components/base/Footer.svelte
+++ src/lib/components/base/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="flex justify-center py-16">
     <div class="border border-black p-3">
       <div class="text-sm flex justify-center">Gonzalo Pelenur</div>
-      <div class="text-sm">me@gonpelenur.com</div>
+      <div class="text-sm flex justify-center">Sunacchi.  &copy; {new Date().getFullYear()}</div>
     </div>
   </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     <main class="mx-auto max-w-2xl space-y-16">
             <h1 class="text-4xl font-bold">@gonpelenur</h1>
             <p class="text-xl">
-              Hello! My name is Gonzalo Pelenur, and I am a Harvard Computer Science student originally from Buenos Aires, Argentina 🇦🇷.
+              Hello! My name is Gonzalo Pelenur, and I am a Harvard Computer Science student originally from Buenos Aires, Argentina 🇦🇷. If you want to contact me, please email <a href="mailto:me@gonpelenur.com"><u>me@gonpelenur.com</u></a>.
 
             </p>
     </main>
