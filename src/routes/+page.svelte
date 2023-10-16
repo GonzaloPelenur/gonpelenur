2c5cff5db4991a75334c17a589823fb2784b8786
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-16 12:03:10 -0400

    new text typewritter animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
 
 <div class="p-d">
     <main class="mx-auto max-w-2xl space-y-16">
-            <h1 class="text-4xl font-bold">@gonpelenur</h1>
+            
+            <div class="typewriter">
+              <h1 class="text-4xl font-bold">@gonpelenur</h1>
+            </div>
             <p class="text-xl">
               Hello! My name is Gonzalo Pelenur, and I am a Harvard Computer Science student originally from Buenos Aires, Argentina 🇦🇷. If you want to contact me, please email <a href="mailto:me@gonpelenur.com"><u>me@gonpelenur.com</u></a>.
 
